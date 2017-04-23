efb8d4d28f26ff32cfbd788c23fbac17abf6b4c3
Andrico <andrico1234@yahoo.co.uk>
2017-04-23 11:45:40 +0100

    add login functoinality

--- server/models/user.js
+++ server/models/user.js
@@ -62,6 +62,34 @@ UserSchema.methods.generateAuthToken = function() {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+
+    var User = this;
+
+    return User.findOne({
+
+        'email': email
+    }).then((user) => {
+
+        if (!user) {
+
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+
+                if (res) {
+
+                    return resolve(user);
+                }
+
+                return reject();
+            });
+        });
+    });
+};
+
 UserSchema.statics.findByToken = function(token) {
 
     var User = this;
