d0ab458bd76f03de371729960166416fc6228be8
Andrico <andrico1234@yahoo.co.uk>
2017-04-27 19:40:40 +0100

    add tests for delete

--- server/server.spec.js
+++ server/server.spec.js
@@ -345,3 +345,29 @@ describe('POST /users/login', () => {
             });
     });
 });
+
+describe('DELETE /users/me/token', () => {
+
+    it('should remove auth token on logout', (done) => {
+
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+
+                if (err) {
+
+                    return done(err)
+                }
+
+                User.find({email: users[0].email}).then((user) => {
+                    expect(user[0].tokens.length).toEqual(0);
+                    done();
+                }).catch((err) => {
+
+                    done(err);
+                });
+            });
+    });
+});
